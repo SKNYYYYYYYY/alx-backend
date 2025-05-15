7944c5de6e32f93d0c33fd24045226931ad95294
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-16 01:22:36 +0300

    fix pycodestyle main

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,19 +5,21 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-	""" a basic caching system
-	"""
-	def put(self, key, item):
-		""" add item to cache
-		"""
-		if key or item is not None:
-			self.cache_data.update({key: item})
-		else:
-			pass
-	def get(self, key):
-		""" retunr the value of the key
-		"""
-		if key is not None:
-			return self.cache_data.get(key)
-		else:
-			return None
+    """   a basic caching system
+    """
+
+    def put(self, key, item):
+        """ add item to cache
+        """
+        if key or item is not None:
+            self.cache_data.update({key: item})
+        else:
+            pass
+
+    def get(self, key):
+        """ retunr the value of the key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        else:
+            return None
