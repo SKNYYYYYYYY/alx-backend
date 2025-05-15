e6c80615afbc8ca61c4d0742a8c6387f3b0f995f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-16 01:29:20 +0300

    fix pycodestyle main

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,7 +11,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ add item to cache
         """
-        if key or item is not None:
+        if key is not None and item is not None:
             self.cache_data.update({key: item})
         else:
             pass
