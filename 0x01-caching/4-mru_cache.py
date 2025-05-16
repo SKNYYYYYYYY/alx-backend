52ae2bb9e3a57cd7c2784b6abdc6803a18f51966
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-16 19:09:16 +0300

    MRU cache policy main

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""MRU caching system"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """implements MRU caching policy"""
+    counter = 0
+
+    def __init__(self):
+        super().__init__()
+        self.keys = {}
+
+    def put(self, key, item):
+        """insert an item to cache"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+            self.keys[key] = MRUCache.counter
+            MRUCache.counter += 1
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            current_value = max(self.keys.values())
+            # find second last value
+            for k in self.keys.values():
+                if k < current_value:
+                    mru_value = k
+            for key, val in self.keys.items():
+                if val == mru_value:
+                    mru_key = key
+            print(f"DISCARD: {mru_key}")
+            del self.cache_data[mru_key]
+            del self.keys[mru_key]
+
+    def get(self, key):
+        """returns the key's item"""
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            self.keys[key] = MRUCache.counter
+            MRUCache.counter += 1
+            return self.cache_data[key]
