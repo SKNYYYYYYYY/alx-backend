bbcf928111c334b380ec1c1148653ac530a6fc99
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-23 22:41:13 +0300

    basic flask app main

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 """basic flask app"""
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template('0-index.html')
+    """renders html file"""
+    return render_template('0-index.html')
