ec8ea675b1eb728e8c72cbb2747864a7578d03a9
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-23 23:39:44 +0300

    Add get_locale to select best language match using request headers main

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app with Babel for internationalization."""
 from flask_babel import Babel
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
@@ -12,3 +12,13 @@ babel = Babel(app)
 def get_locale():
     """return locale"""
     return request.accept_languages.best_match(["en", "fr"])
+
+
+@app.route("/")
+def index():
+    """renders html file"""
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
